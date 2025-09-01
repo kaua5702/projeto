193cdac4ecd99e17b32d29b019c9538ad005c3b6
Kaua Biscotto <kaua.biscotto1405@gmail.com>
2025-09-01 18:39:11 -0300

    continuação

---
+++ aula08.py
@@ -0,0 +1,6 @@
+import math
+
+num = int(input('Digite um número inteiro: '))
+raiz = math.sqrt(num)
+
+print(f'A raiz quadrada de {num} é {raiz:.2f}.')
--- aula8.py
+++
@@ -1,4 +0,0 @@
-import math
-
-num = int(input('Digite um número inteiro: '))
-raiz = math.sqrt(num)
